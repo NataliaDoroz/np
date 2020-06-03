dcb3371bcdd2155a6cbe4c24855acbdf723e8f15
NataliaD <natalia123doroz@gmail.com>
2020-06-03 22:21:35 +0200

    poprawka

--- src/Homework01a.java
+++ src/Homework01a.java
@@ -1,7 +1,7 @@
 public class Homework01a {
     public static void main(String[] args) {
         System.out.println("HeWo123");
-        System.out.println("HeWo123");
+        System.out.println("HeWo12345");
 
     }
 }
